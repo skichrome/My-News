a50101bbdf3837a4ff7c5c8a88906682017b8afd
skichrome <toni.campeol@outlook.fr>
2018-03-21 15:20:13 +0100

    Completed the test with variables and methods

---
+++ app/src/test/java/com/skichrome/mynews/ModelTopStoriesAPIUnitTest.java
@@ -0,0 +1,71 @@
+package com.skichrome.mynews;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * This test class assert that all fields
+ */
+
+public class ModelTopStoriesAPIUnitTest
+{
+    private List<Result> mResultsList;
+    private List<Multimedium> mMultimedium;
+
+    @Before
+    public void setVariables()
+    {
+        mResultsList = new ArrayList<>();
+        mMultimedium = new ArrayList<>();
+
+        //set a test result array, with test defined fields
+        Result mTempResult = new Result();
+        mTempResult.setUrl("http://www.example.fr");
+        mTempResult.setTitle("An amazing title of this article");
+        mTempResult.setCreatedDate("01012017");
+
+        //set a multimedium test array
+        Multimedium mTempMultimedium = new Multimedium();
+        mTempMultimedium.setUrl("http://www.the-amazin-image-url.fr");
+        mTempMultimedium.setHeight(75);
+        mTempMultimedium.setWidth(75);
+        mTempMultimedium.setCaption("amazing description of image");
+        mMultimedium.add(mTempMultimedium);
+        mMultimedium.add(mTempMultimedium);
+
+        mTempResult.setMultimedia(mMultimedium);
+
+        //add two items to the list
+        mResultsList.add(mTempResult);
+        mResultsList.add(mTempResult);
+    }
+
+    @Test
+    public void shouldTheTopStoriesAPIModelReturnAllNeededValues()
+    {
+        MainNewYorkTimesTopStories mMainNewYorkTimesTopStories = new MainNewYorkTimesTopStories();
+        mMainNewYorkTimesTopStories.setResults(mResultsList);
+
+        for (Result mResult : mMainNewYorkTimesTopStories.getResults())
+        {
+            assertThat("all variables must be present in the model fields", mResult.getUrl() != null);
+            assertThat("all variables must be present in the model fields", mResult.getTitle() != null);
+            assertThat("all variables must be present in the model fields", mResult.getCreatedDate() != null);
+            assertThat("all variables must be present in the model fields", mResult.getMultimedia() != null);
+            for (Multimedium mMultimedium : mResult.getMultimedia())
+            {
+                assertThat("all variables must be present in the model fields", mMultimedium.getUrl() != null);
+                assertThat("all variables must be present in the model fields", mMultimedium.getHeight() != null);
+                assertThat("all variables must be present in the model fields", mMultimedium.getWidth() != null);
+                assertThat("all variables must be present in the model fields", mMultimedium.getCaption() != null);
+            }
+        }
+    }
+}
