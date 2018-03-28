40043eb1909766fef580c7f02b13be65acc3f43b
skichrome <toni.campeol@outlook.fr>
2018-03-28 16:38:09 +0200

    [Added] new activity for notifications management and article search

---
+++ app/src/main/java/com/skichrome/mynews/controller/activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.skichrome.mynews.controller.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.skichrome.mynews.R;
+
+public class SearchActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
